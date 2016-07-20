5b3398076e80a350974adc6fca6bc48b7202bcb0
Curtis Alexander <calexander@elevate.com>
2016-07-20 18:12:31 -0500

    formatting

--- wrapper.sh
+++ wrapper.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 /path/to/script [parameters]
+
 # http://tldp.org/LDP/abs/html/exit-status.html
 # $? ==> reads the exit status of the last command executed
 # $? -eq 0 is success
 # $? -ne 0 is failure
+
 if [ $? -ne 0 ]; then
     send-email.py --config failure.yaml
 else
